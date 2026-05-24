2bdf49e643c9683e7b77241fc1e4ed9d7399ef60
Umar91123 <umarfun12334@gmail.com>
2026-05-24 23:31:59 +0500

    Update axios.js

--- frontEnd/src/api/axios.js
+++ frontEnd/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api= axios.create({
-  baseURL:'http://localhost:5001/api'
+  baseURL:'https://mern-ecommerce-platform-1vft.onrender.com/api'
 })
 
 export default api
